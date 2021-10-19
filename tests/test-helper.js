4106644256e38fbcfd61856f94b4c98dd27c2b71
Christopher Korbel <ckorbel1@gmail>
2021-10-19 13:32:39 -0700

    first commint

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'not-react/app';
+import config from 'not-react/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
